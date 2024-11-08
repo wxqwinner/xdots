07b99c550d44a311c1e9e755832f2178537036fa
wxqwinner <wxqwinner@gmail.com>
2024-11-08 13:15:20 +0800

    update

--- config/ags/widgets/resttime/main.js
+++ config/ags/widgets/resttime/main.js
@@ -2,6 +2,8 @@ import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 
 const WINDOW_NAME = "resttime"
 
+// 检测键盘鼠标活动，连续活动50分钟，休息10分钟
+// 
 
 export const resttime = Widget.Window({
     name: WINDOW_NAME,
