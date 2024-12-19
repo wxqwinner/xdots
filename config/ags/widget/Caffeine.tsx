0877086dcb287bf3336bba2f9a7bef4515a7eb1d
wxqwinner <wxqwinner@gmail.com>
2024-12-19 11:51:50 +0800

    add caffeine

--- config/ags/widget/Caffeine.tsx
+++ config/ags/widget/Caffeine.tsx
@@ -3,27 +3,23 @@ import { exec, execAsync } from "astal/process"
 
 export default function Caffeine() {
     let isCaffeineOn = false;
-    // let status = Gtk.Widget.Icon({
-    //         icon: "caffeine_off"
-    //     })
+    let statusIcon = <icon icon={"caffeine_off"} />;
 
     execAsync(`hyprctl dispatch exec hypridle`);
 
     function updateCaffeineStatus() {
         if (isCaffeineOn) {
-            // icon.icon = 'caffeine_on'
+            statusIcon.icon = 'caffeine_on'
             execAsync(`killall hypridle`);
         } else {
-            // icon.icon = 'caffeine_off'
+            statusIcon.icon = 'caffeine_off'
             execAsync(`hyprctl dispatch exec hypridle`);
         }
     }
 
     return <box>
-        <icon icon={"caffeine_off"} />
-        <button
-            onClicked={() => {isCaffeineOn = !isCaffeineOn;updateCaffeineStatus();}}
-        >
+        <button onClicked={() => {isCaffeineOn = !isCaffeineOn;updateCaffeineStatus();}}>
+            {statusIcon}
         </button>
     </box>
 }
