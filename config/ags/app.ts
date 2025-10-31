1d7f345e7948c89919f00c1f6da4c4a348fa07e6
wxqwinner <wxqwinner@gmail.com>
2025-10-31 14:36:51 +0800

    monitor set

--- config/ags/app.ts
+++ config/ags/app.ts
@@ -2,9 +2,16 @@ import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
 
+
+function main() {
+    for (const monitor of app.get_monitors()) {
+        if (monitor.model == "0x08DF") {
+            Bar(monitor)
+        }
+    }
+}
+
 app.start({
-  css: style,
-  main() {
-    app.get_monitors().map(Bar)
-  },
+    css: style,
+    main
 })
